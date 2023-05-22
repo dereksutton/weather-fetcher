4f70f9f3918a157250942c3e373d5c17c79d7ed8
Derek Sutton <dereksutton86@gmail.com>
2023-05-22 16:18:32 -0400

    changed class names in forecast board to render proper data, added forecast fetch/render functions in script.js

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,11 @@ const tempNow = document.getElementById('temp-now');
 const humidNow = document.getElementById('humid-now');
 const windNow = document.getElementById('wind-now');
 const weatherIcon = document.getElementById('icon-now');
+const dayZero = document.getElementById('day-zero');
+const dayOne = document.getElementById('day-one');
+const dayTwo = document.getElementById('day-two');
+const dayThree = document.getElementById('day-three');
+const dayFour = document.getElementById('day-four');
 const currentWeatherDisplay = document.getElementById('current-weather');
 const searchHistory = document.getElementById('cities-searched');
 
@@ -35,8 +40,58 @@ function fetchTheWeather(city) {
     });
 }
 
+function fetchForecast(city) {
+    var forecastURL = apiURL + "/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
+    fetch(forecastURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            // call renderForecast function to render the data
+            renderForecast(data);
+        });
+}
+
+function renderForecast(data) {
+    // Get all divs for the days
+    const dayDivs = [
+        dayZero,
+        dayOne,
+        dayTwo,
+        dayThree,
+        dayFour
+    ];
+
+    // Loop over each div and set the data
+    for (var i = 0; i < dayDivs.length; i++) {
+        var dayDiv = dayDivs[i];
+        var forecast = data.list[i * 8];
+
+        // Set the date in the h5 element
+        var dateElement = dayDiv.querySelector('h5');
+        var date = new Date(forecast.dt * 1000);
+        dateElement.textContent = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+
+        // Set the weather icon
+        var iconElement = dayDiv.querySelector('img');
+        var iconCode = forecast.weather[0].icon;
+        iconElement.src = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+        // Set the temperature
+        var tempElement = dayDiv.querySelector('.temp-p');
+        tempElement.textContent = 'Temp: ' + forecast.main.temp + ' °F';
+
+        // Set the humidity
+        var humidElement = dayDiv.querySelector('.humid-p');
+        humidElement.textContent = 'Humidity: ' + forecast.main.humidity + '%';
+    }
+}
+
 searchButton.addEventListener('click', function(event) {
     event.preventDefault();
     var city = searchInput.value;
     fetchTheWeather(city);
+    fetchForecast(city);
 });
